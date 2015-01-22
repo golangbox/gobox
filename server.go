f5664ca3186c7909fd8361c7ac284615478e262d
Fernando Andrade <fernando.neto@gmail.com>
2015-01-22 17:30:21 -0500

    server v0.1

---
+++ server.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+    "html/template"
+    "encoding/json"
+    "io"
+    "net/http"
+    "os"    
+    "log"
+)
+
+type File struct {
+    name string
+    hash string
+    size int64
+}
+
+
+var f1 = File{"file1", "sjdalkjsda", 1234}
+var f2 = File{"file2", "lkjlkjlkl", 4321}
+var allFiles = map[string]File{"file1":f1, "file2":f2}
+
+//Compile templates on start
+var templates = template.Must(template.ParseFiles("templates/upload.html"))
+
+//Display the named template
+func display(w http.ResponseWriter, tmpl string, data interface{}) {
+    templates.ExecuteTemplate(w, tmpl+".html", data)
+}
+
+func fileListHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    json.NewEncoder(w).Encode(allFiles)
+}
+
+//This is where the action happens.
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+    switch r.Method {
+    //GET displays the upload form.
+    case "GET":
+        log.Println("Get...")
+        display(w, "upload", nil)
+
+    //POST takes the uploaded file(s) and saves it to disk.
+    case "POST":
+        //get the multipart reader for the request.
+        reader, err := r.MultipartReader()
+
+        log.Println("Receiving stuff...")
+        hash := r.FormValue("sha-1")
+
+
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        //copy each part to destination.
+        for {
+            part, err := reader.NextPart()
+            if err == io.EOF {
+                break
+            }
+
+            //if part.FileName() is empty, skip this iteration.
+            if part.FileName() == "" {
+                continue
+            }
+
+            dst, err := os.Create("/Users/partec/hackerschool/go/gobox/tmp/" + part.FileName())
+            defer dst.Close()
+
+            allFiles[]
+
+            if err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+            }
+            
+            if _, err := io.Copy(dst, part); err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+            }
+        }
+        //display success message.
+        display(w, "upload", "Upload successful.")
+    default:
+        w.WriteHeader(http.StatusMethodNotAllowed)
+    }
+}
+
+// filename, size, sha-1
+
+func main() {
+    log.Println("Server up...")
+
+    http.HandleFunc("/upload", uploadHandler)
+
+    http.HandleFunc("/files", fileListHandler)
+
+    //static file handler.
+    http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+    //Listen on port 8080
+    http.ListenAndServe(":8080", nil)
+}
