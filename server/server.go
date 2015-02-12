4e3dcf9447dbb6c84bb968cbb455fe39c1530c74
Marin Alcaraz <marin.alcaraz@gmail.com>
2015-02-12 18:22:58 -0500

    server udp integration completed, missing auth!

--- server/server.go
+++ server/server.go
@@ -1,7 +1,60 @@
 package main
 
-import "github.com/golangbox/gobox/server/api"
+import (
+	"fmt"
+
+	"github.com/golangbox/gobox/UDPush"
+)
+
+type services struct {
+	s3     bool
+	api    bool
+	udpush bool
+}
+
+type server struct {
+	services
+	name        string
+	ip          string
+	port        uint
+	clientLimit uint
+	status      func() bool
+	display     func() string
+	pusher      UDPush.Pusher
+}
+
+func (s *server) checkStatus() bool {
+	if s.services.api == true &&
+		s.services.s3 == true &&
+		s.services.udpush == true {
+		return true
+	}
+	return false
+}
 
 func main() {
-	api.ServeServerRoutes()
+
+	//Launch API
+	//api.ServeServerRoutes()
+
+	s := server{
+		name:        "Elvis",
+		ip:          "127.0.0.1",
+		port:        4242,
+		clientLimit: 10,
+	}
+
+	////Launch UDP notification service
+	////Define the Subject (The guy who is goin to hold all the clients)
+
+	s.pusher = UDPush.Pusher{
+		ServerID: s.ip,
+		BindedTo: s.port,
+	}
+
+	err := s.pusher.InitUDPush()
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
